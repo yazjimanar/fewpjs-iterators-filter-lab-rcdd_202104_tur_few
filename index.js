34aa1876abca3c018e31fb6a7f491359b05743af
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-06-22 16:22:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(list, name) {
+    return list.filter(function (driverName) {
+        return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(list, partialName) {
+    let lengthOfName = partialName.length;
+    return list.filter(function (driverName) {
+        return driverName.slice(0, lengthOfName) === partialName;
+    });
+}
+
+function matchName(list, name) {
+    return list.filter(function (driver) {
+        return driver.name.toLowerCase() === name.toLowerCase();
+    });
+}
+
+console.log(matchName(drivers, "Test"))
+
+console.log(findMatching(drivers, "TTeess"))
+console.log(fuzzyMatch(drivers, "Z"))
